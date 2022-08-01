903abe8052f333020221ecbce2d92f647317aade
seregam09 <seregam09@gmail.com>
2022-08-01 18:26:15 +0300

    beta version

---
+++ generation-web-conf.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DN=`cat ${PWD}/.env | grep DOMAIN_NAME`
+
+echo "server {
+    server_name ${DN} www.${DN}
+    listen 80;
+    root /public_html;
+
+    # Letsencrypt velidation
+    location ~ /.well-known/acme-challege {
+        allow all;
+        root /var/www/certbot;
+    }
+
+    # Redirect http to https
+    location / {
+        return 301 https://${DN}\$request_uri;
+    }
+}
+
+server {
+    server_name ${DN} www.${DN}
+    listen 443 ssl http2;
+    root /public_html;
+
+    ssl on;
+    ssl_certificate     /etc/nginx/ssl/live/${DN}/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/live/${DN}/privkey.pem;
+    ssl_dhparam         /etc/nginx/ssl/ssl-dhparams.pem;
+
+    include /etc/nginx/ssl/options-ssl-nginx.conf;
+    
+    location / {
+        index index.html;
+    }
+}
+" > ./data/conf.d/${DN}.conf
